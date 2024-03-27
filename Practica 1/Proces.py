45c0cc731dce9f5305e01feae92486195285e14a
Hugo Ernesto Sosa <hsosa@cnea.gov.ar>
2024-03-27 12:04:14 -0300

    se plotean histogramas como lineas y se extraen los arrays

--- Practica 1/Proces.py
+++ Practica 1/Proces.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-time0,picos0=np.loadtxt('Practica 1\\137Cs_1_resultados.txt', skiprows=1, unpack=True, delimiter=',')
 plt.figure()
+time0,picos0=np.loadtxt('Practica 1\\137Cs_1_resultados.txt', skiprows=1, unpack=True, delimiter=',')
+spectrum_137Cs=plt.hist(picos0, bins=500, histtype='step',label = '137Cs')
+time0,picos0=np.loadtxt('Practica 1\\60Co_1_resultados.txt', skiprows=1, unpack=True, delimiter=',')
+spectrum_60Co=plt.hist(picos0, bins=500, histtype='step',label = '60Co')
 plt.title('Amplitud de los picos', fontsize = 14)
-plt.hist(picos0, bins=500, label = 'data')
 plt.xlabel('Tensión [V]', fontsize = 14)
 plt.ylabel('Cuentas', fontsize = 14)
 plt.yscale("log")
