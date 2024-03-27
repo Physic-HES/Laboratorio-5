f0a34256dad7af2428b47a3978db22428ea249e2
Hugo Ernesto Sosa <hsosa@cnea.gov.ar>
2024-03-27 12:37:12 -0300

    se agrega grid, pendiente aproximada de calibracion, numero de eventos en el titulo y se cambia la legenda del eje x

--- Practica 1/Proces.py
+++ Practica 1/Proces.py
@@ -2,12 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 plt.figure()
+plt.grid()
 time0,picos0=np.loadtxt('Practica 1\\137Cs_1_resultados.txt', skiprows=1, unpack=True, delimiter=',')
-spectrum_137Cs=plt.hist(picos0, bins=500, histtype='step',label = '137Cs')
+spectrum_137Cs=plt.hist(picos0*662/3.4, bins=1000, histtype='step',label = '137Cs')
 time0,picos0=np.loadtxt('Practica 1\\60Co_1_resultados.txt', skiprows=1, unpack=True, delimiter=',')
-spectrum_60Co=plt.hist(picos0, bins=500, histtype='step',label = '60Co')
-plt.title('Amplitud de los picos', fontsize = 14)
-plt.xlabel('Tensión [V]', fontsize = 14)
+spectrum_60Co=plt.hist(picos0*662/3.4, bins=1000, histtype='step',label = '60Co')
+plt.title(f'Amplitud de los picos para {len(picos0)} eventos', fontsize = 14)
+plt.xlabel('Energía [keV]', fontsize = 14)
 plt.ylabel('Cuentas', fontsize = 14)
 plt.yscale("log")
 plt.legend(fontsize = 14)
