74bef8db30e86cbe440abbba4fc3774ffbbc1c54
hugo_sosa <physic>
2024-06-23 18:42:15 -0300

    Calculo de fuerza

--- Practica 3 Pinzas opticas/tracker_partic.py
+++ Practica 3 Pinzas opticas/tracker_partic.py
@@ -56,6 +56,7 @@ def track_points(video_file, quality_level=0.2, max_points=500, screen_region=No
     n_frames = query_video_properties(video_file)['n_frames']
     print(n_frames)
     success, prev_frame = capture.read()
+    print(prev_frame.shape)
     if not success:
         print("Error al leer el primer frame del video")
         return None
@@ -95,6 +96,7 @@ def track_points(video_file, quality_level=0.2, max_points=500, screen_region=No
         image = cv.add(frame, mask)
         cv_draw_text(image, (20, 20),
                      f'tracked points: {len(tracked_points)}')
+        #cv.rectangle(image, (int(1248*3/5-150-40),int(1024*2/3-100)), (int(1248*3/5+150),int(1024*2/3+50)), color=(255,0,0), thickness=15) 
         cv.imshow("track_points", image)
         cv.waitKey(1)
         prev_frame_gray = frame_gray.copy()
@@ -115,30 +117,32 @@ def track_points(video_file, quality_level=0.2, max_points=500, screen_region=No
     result += point_holder.tolist()
     return result
 
+def smd(lista,fps,R):
+    cant=[]
+    for k in range(len(lista)):
+        cant.append(len(lista[k][0]))
+    r_s=np.zeros((np.max(cant),len(lista)))
+    SMD=np.zeros(len(r_s[:,0]))
+    for k in range(len(lista)):
+        x,y=np.array(lista[k][0])*9.375*1E-8,np.array(lista[k][1])*9.375*1E-8
+        r_s[:len(lista[k][0]),k]=np.cumsum((x-x[0])**2+(y-y[0])**2)/(np.arange(len(lista[k][0]))+1)
+    for k in range(3,len(r_s[:,0])):
+        #print(k,np.max(cant),r_s[k,:])
+        SMD[k]=1/np.count_nonzero(r_s[k,:])*np.sum(r_s[k,:])
+    tiempo=(np.arange(np.max(cant))+1)*1/fps
+    lin_coef=np.polyfit(tiempo,SMD,1)
+    eta=2*1.38E-23*(273.15+13)/(3*np.pi*lin_coef[0]*R)
+    part_rsmax=np.argmax(r_s[-1,:])
+    return tiempo, SMD, eta, part_rsmax
+
+
 def graficar(file,path,R):
     eta_eff=[]
     for k in range(len(file)):
         ruta = path+file[k]
+        print(ruta)
         lista = track_points(ruta, quality_level=0.10, max_points=100, screen_region=None, tolerance=20)
-
-        def smd(lista,fps,R):
-            cant=[]
-            for k in range(len(lista)):
-                cant.append(len(lista[k][0]))
-            r_s=np.zeros((np.max(cant),len(lista)))
-            SMD=np.zeros(len(r_s[:,0]))
-            for k in range(len(lista)):
-                x,y=np.array(lista[k][0])*9.375*1E-8,np.array(lista[k][1])*9.375*1E-8
-                r_s[:len(lista[k][0]),k]=np.cumsum((x-x[0])**2+(y-y[0])**2)/(np.arange(len(lista[k][0]))+1)
-            for k in range(3,len(r_s[:,0])):
-                #print(k,np.max(cant),r_s[k,:])
-                SMD[k]=1/np.count_nonzero(r_s[k,:])*np.sum(r_s[k,:])
-            tiempo=(np.arange(np.max(cant))+1)*1/fps
-            lin_coef=np.polyfit(tiempo,SMD,1)
-            eta=2*1.38E-23*(273.15+13)/(3*np.pi*lin_coef[0]*R)
-            part_rsmax=np.argmax(r_s[-1,:])
-            return tiempo, SMD, eta, part_rsmax
-
+        
         tiempo, SMD, eta, part=smd(lista,15,R)
         print(f'Viscosidad: eta={eta}')
         plt.figure(1)
@@ -161,53 +165,76 @@ def graficar(file,path,R):
     return eta_eff
 
 
+def fuerza(lista,fps,R,eta,tam_im):
+    cant=[]
+    for k in range(len(lista)):
+        cant.append(len(lista[k][0]))
+    v=np.zeros((np.max(cant)-1,len(lista)))
+    vel=np.zeros(np.max(cant)-1)
+    vel_pin=np.zeros_like(vel)
+    ind_pin=[]
+    ind_unpin=[]
+    for k in range(len(lista)):
+        x,y=np.array(lista[k][0])*9.375*1E-8,np.array(lista[k][1])*9.375*1E-8
+        v[:len(lista[k][0])-1,k]=np.diff(np.sqrt((x-x[0])**2+(y-y[0])**2))*fps
+        if (np.abs(np.array(lista[k][1])-(tam_im[0]*2/3-25)).max()<75 and np.abs(np.array(lista[k][0])-(tam_im[1]*3/5-20)).max()<170):
+            ind_pin.append(k)
+        else:
+            ind_unpin.append(k)
+    print(ind_pin)
+    N1_=[]
+    for m in ind_pin:
+        N1_.append(np.count_nonzero(v[:,m]))
+    N1=np.max(N1_)
+    N2_=[]
+    for m in ind_unpin:
+        N2_.append(np.count_nonzero(v[:,m]))
+    N2=np.max(N2_)
+    for j in range(1,N1):
+        vel[j-1] = 1/np.count_nonzero(v[j,ind_unpin])*np.sum(v[j,ind_unpin])
+    for j in range(1,N2):
+        vel_pin[j-1] = 1/np.count_nonzero(v[j,ind_pin])*np.sum(v[j,ind_pin])
+    ind_acel=np.argmax(np.diff(vel_pin)*1/fps)
+    fuer=6*np.pi*eta*R*vel[ind_acel]
+    tiempo = (np.arange(len(lista[k][0])-1)+1)*1/fps
+    plt.plot(tiempo,np.abs(vel)*1E6,label=r'$V_{prom}$ Particulas libres')
+    plt.plot(tiempo,np.abs(vel_pin)*1E6,label=r'$V$ Particula sujeta')
+    plt.plot(tiempo[ind_acel],np.abs(vel[ind_acel])*1E6,'.r',label=r'$V$ para calcular $F$')
+    plt.legend()
+    return fuer
+
+
 def Fuerza_Stoks(file,path,R,eta):
     F=[]
     for k in range(len(file)):
         ruta = path+file[k]
         lista = track_points(ruta, quality_level=0.10, max_points=100, screen_region=None, tolerance=20)
 
-        plt.figure()
-        plt.ylabel('Velocidad [m/s]')
-        plt.xlabel('Tiempo [s]')
+        plt.figure(k+1)
+        plt.ylabel(r'Velocidad [$\mu m/s$]')
+        plt.xlabel(r'Tiempo [$s$]')
         plt.title(file[k])
-        def fuerza(lista,fps,R,eta):
-            cant=[]
-            for k in range(len(lista)):
-                cant.append(len(lista[k][0]))
-            v=np.zeros((np.max(cant)-1,len(lista)))
-            ax=np.zeros((np.max(cant)-2,len(lista)))
-            for k in range(len(lista)):
-                x,y=np.array(lista[k][0])*9.375*1E-8,np.array(lista[k][1])*9.375*1E-8
-                v[:len(lista[k][0])-1,k]=np.diff(np.sqrt((x-x[0])**2+(y-y[0])**2))*fps
-                ax[:len(lista[k][0])-2,k]=np.diff(np.diff(x-x[0])*fps)*fps
-                tiempo = (np.arange(len(lista[k][0])-1)+1)*1/fps
-                velx = np.diff(x-x[0])*fps
-                plt.plot(tiempo,velx)
-            ind = np.unravel_index(np.argmax(np.abs(ax), axis=None), ax.shape)
-            vel = np.mean(v[ind[0],np.arange(v.shape[1])!=ind[1]])
-            fuer=6*np.pi*eta*R*vel
-            return fuer
 
-        fuer = fuerza(lista,15,R,eta)
+        fuer = fuerza(lista,15,R,eta,[1024,1280])
         F.append(fuer)
-        print(f'Fuerza={F[-1]:.3} N')
-        plt.show()
+        print(f'Fuerza={F[-1]*1E12:.3} pN')
+    print(f'Fuerza Promedio = {np.mean(F)*1E12:.3} +- {np.std(F)*1E12:.2} pN')
+    plt.show()
     return F
         
 # Muestras con silica de 0.5 micrones
-#path='Practica 3 Pinzas opticas\\mov browneano muestra2 05um\\'
+#path='/home/hugo_sosa/Documents/L5/Laboratorio-5/Practica 3 Pinzas opticas/mov browneano muestra2 05um/'
 #file=['01_muestra2_0.avi','01_muestra2_1.avi','01_muestra2_2.avi','01_muestra2_3.avi']
 #eta_05=graficar(file,path,0.5E-6)
 
 # Muestras con latex de 4.5 micrones
-path='Practica 3 Pinzas opticas\\mov_browneano_agua_45um\\'
+path='/home/hugo_sosa/Documents/L5/Laboratorio-5/Practica 3 Pinzas opticas/mov_browneano_agua_45um/'
 #file=['01_muestra0_0.avi','01_muestraB_0.avi','01_muestraC_0.avi','01_muestraD_0.avi','01_muestraE_0.avi']
 file=['01_muestraE_0.avi']
 eta_45=graficar(file,path,4.5E-6)
 
 # Muestras con latex de 4.5 micrones al sujetar con pinza optica
-path='Practica 3 Pinzas opticas\\'
+path='/home/hugo_sosa/Documents/L5/Laboratorio-5/Practica 3 Pinzas opticas/'
 file=['Arrastre_muestraE_1.avi','Arrastre_muestraE_2.avi','Arrastre_muestraE_3.avi','Arrastre_muestraE_4.avi']
 F=Fuerza_Stoks(file,path,4.5E-6,eta_45[-1])
 
